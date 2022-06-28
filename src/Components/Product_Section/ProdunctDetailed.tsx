1714837ac9250c96161e027c1eb444878a0a49d8
Kena737 <singhamitsboss@gmail.com>
2022-06-28 17:01:46 -0400

    updated

--- src/Components/Product_Section/ProdunctDetailed.tsx
+++ src/Components/Product_Section/ProdunctDetailed.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@nextui-org/react'
 import React from 'react'
 
 const ProdunctDetailed = () => {
@@ -15,10 +16,11 @@ const ProdunctDetailed = () => {
                 <div className='pt-5'>
                     <img height="56px" width="56px" src="https://bit.ly/3a0IVUB" alt="" />
                 </div>
+                <hr />
             </div>
-            <hr />
-            <div className='flex mx-32 my-10 '>
-                <div>
+
+            <div className='flex mx-32 my-7 flex-col'>
+                <div className='my-3'>
                     <div className='flex items-center'>
                         <span>
                             <svg className='inline-block h-[24px] w-[24px] fill-current' viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" >
@@ -27,19 +29,52 @@ const ProdunctDetailed = () => {
                         </span>
                         <h3 className='inline-block text-xl font-medium mx-4'>Dive right in</h3>
                     </div>
-                    <p className='ml-11 text-gray-600 font-[Poppins]'>This is one of the few places in the area with a pool.</p>
+                    <p className='text-left ml-11 text-gray-600 font-[Poppins]'>This is one of the few places in the area with a pool.</p>
                 </div>
 
-                <div>
+                <div className='my-3'>
                     <div className='flex items-center'>
                         <span>
-                            <svg className='inline-block h-[24px] w-[24px] fill-current' viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" >
-                                <path d="M24 26c.988 0 1.945.351 2.671 1.009.306.276.71.445 1.142.483L28 27.5v2l-.228-.006a3.96 3.96 0 0 1-2.443-1.003A1.978 1.978 0 0 0 24 28c-.502 0-.978.175-1.328.491a3.977 3.977 0 0 1-2.67 1.009 3.977 3.977 0 0 1-2.672-1.009A1.978 1.978 0 0 0 16 28c-.503 0-.98.175-1.329.491a3.978 3.978 0 0 1-2.67 1.009 3.978 3.978 0 0 1-2.672-1.008A1.978 1.978 0 0 0 8 28c-.503 0-.98.175-1.33.491a3.96 3.96 0 0 1-2.442 1.003L4 29.5v-2l.187-.008a1.953 1.953 0 0 0 1.142-.483A3.975 3.975 0 0 1 8 26c.988 0 1.945.352 2.671 1.009.35.316.826.49 1.33.491.502 0 .979-.175 1.328-.492A3.974 3.974 0 0 1 16 26c.988 0 1.945.351 2.671 1.009.35.316.826.49 1.33.491.502 0 .979-.175 1.328-.491A3.975 3.975 0 0 1 23.999 26zm0-5c.988 0 1.945.351 2.671 1.009.306.276.71.445 1.142.483L28 22.5v2l-.228-.006a3.96 3.96 0 0 1-2.443-1.003A1.978 1.978 0 0 0 24 23c-.502 0-.978.175-1.328.491a3.977 3.977 0 0 1-2.67 1.009 3.977 3.977 0 0 1-2.672-1.009A1.978 1.978 0 0 0 16 23c-.503 0-.98.175-1.329.491a3.978 3.978 0 0 1-2.67 1.009 3.978 3.978 0 0 1-2.672-1.008A1.978 1.978 0 0 0 8 23c-.503 0-.98.175-1.33.491a3.96 3.96 0 0 1-2.442 1.003L4 24.5v-2l.187-.008a1.953 1.953 0 0 0 1.142-.483A3.975 3.975 0 0 1 8 21c.988 0 1.945.352 2.671 1.009.35.316.826.49 1.33.491.502 0 .979-.175 1.328-.492A3.974 3.974 0 0 1 16 21c.988 0 1.945.351 2.671 1.009.35.316.826.49 1.33.491.502 0 .979-.175 1.328-.491A3.975 3.975 0 0 1 23.999 21zM20 3a4 4 0 0 1 3.995 3.8L24 7v2h4v2h-4v5c.912 0 1.798.3 2.5.862l.171.147c.306.276.71.445 1.142.483L28 17.5v2l-.228-.006a3.96 3.96 0 0 1-2.443-1.003A1.978 1.978 0 0 0 24 18c-.502 0-.978.175-1.328.491a3.977 3.977 0 0 1-2.67 1.009 3.977 3.977 0 0 1-2.672-1.009A1.978 1.978 0 0 0 16 18c-.503 0-.98.175-1.329.491a3.978 3.978 0 0 1-2.67 1.009 3.978 3.978 0 0 1-2.672-1.008A1.978 1.978 0 0 0 8 18c-.503 0-.98.175-1.33.491a3.96 3.96 0 0 1-2.442 1.003L4 19.5v-2l.187-.008a1.953 1.953 0 0 0 1.142-.483A3.975 3.975 0 0 1 8 16c.988 0 1.945.352 2.671 1.009.35.316.826.49 1.33.491.502 0 .979-.175 1.328-.492a3.956 3.956 0 0 1 2.444-1.002L16 16v-5H4V9h12V7a2 2 0 0 0-3.995-.15L12 7h-2a4 4 0 0 1 7-2.645A3.985 3.985 0 0 1 20 3zm-2 13.523c.16.091.313.194.459.307l.212.179c.35.316.826.49 1.33.491.439 0 .86-.134 1.191-.38l.137-.111c.206-.187.431-.35.67-.486V11h-4zM20 5a2 2 0 0 0-1.995 1.85L18 7v2h4V7a2 2 0 0 0-2-2z"></path>
+                            <svg className='inline-block h-[24px] w-[24px] fill-current' viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false">
+                                <path d="M14.998 1.032a2 2 0 0 0-.815.89l-3.606 7.766L1.951 10.8a2 2 0 0 0-1.728 2.24l.031.175A2 2 0 0 0 .87 14.27l6.36 5.726-1.716 8.608a2 2 0 0 0 1.57 2.352l.18.028a2 2 0 0 0 1.215-.259l7.519-4.358 7.52 4.358a2 2 0 0 0 2.734-.727l.084-.162a2 2 0 0 0 .147-1.232l-1.717-8.608 6.361-5.726a2 2 0 0 0 .148-2.825l-.125-.127a2 2 0 0 0-1.105-.518l-8.627-1.113-3.606-7.765a2 2 0 0 0-2.656-.971zm-3.07 10.499l4.07-8.766 4.07 8.766 9.72 1.252-7.206 6.489 1.938 9.723-8.523-4.94-8.522 4.94 1.939-9.723-7.207-6.489z"></path>
                             </svg>
                         </span>
-                        <h3 className='inline-block text-xl font-medium mx-4'>Dive right in</h3>
+                        <h3 className='inline-block text-xl font-medium mx-4'>Experienced host</h3>
                     </div>
-                    <p className='ml-11 text-gray-600 font-[Poppins]'>This is one of the few places in the area with a pool.</p>
+                    <p className='text-left ml-11 mb-7 text-gray-600 font-[Poppins]'>Dorothy has 726 reviews for other places.</p>
+                </div>
+                <hr />
+            </div>
+            {/* aircover section */}
+            <div className='mx-32 my-7'>
+                <div className='my-7'>
+                    <img src="https://bit.ly/3nmohkZ" className='w-[123.16px] h-[26px]' alt="" />
+                </div>
+                <div>
+                    <p className='text-left mt-5 text-lg text-gray-800 '>Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.</p>
+                    <h3 className='text-left my-3 text-xl font-medium underline'>Learn more</h3>
+                </div>
+            </div>
+            <div className='mx-32 my-7'>
+                <hr />
+                <p className='text-left mt-5 text-lg text-gray-800 '>Adaaran Club Rannalhi is featured among the best hotels in Maldives and sits exclusively at the tip of the South Male atoll within the exotic collection of islands known as the Maldives. Its unique location offers access to pristine beaches, excellent scuba diving opportunities and a relaxed environment with easy access to the capital city of Male.</p>
+                <div>
+                </div>
+            </div>
+            <div className='mx-32 my-7'>
+                <hr />
+                <h1 className='text-left my-10 text-2xl font-semibold text-gray-900'>Where you'll sleep</h1>
+
+                <div className='border border-gray-300 rounded-xl justify-start flex flex-col p-2 w-56'>
+                    <span className=' px-5 py-5'>
+                        <svg className='h-[24px] w-[24px] fill-current' viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false">
+                            <path d="M26 4a2 2 0 0 1 1.995 1.85L28 6v7.839l1.846 5.537a3 3 0 0 1 .115.468l.03.24.009.24V30h-2v-2H4v2H2v-9.675a3 3 0 0 1 .087-.717l.067-.232L4 13.836V6a2 2 0 0 1 1.697-1.977l.154-.018L6 4zm2 18H4v4h24zm-1.388-6H5.387l-1.333 4h23.891zM26 6H6v8h2v-4a2 2 0 0 1 1.85-1.995L10 8h12a2 2 0 0 1 1.995 1.85L24 10v4h2zm-11 4h-5v4h5zm7 0h-5v4h5z"></path>
+                        </svg>
+                    </span>
+                    <h4 className='text-left px-5 font-semibold text-lg'>Bedroom</h4>
+                    <p className='text-left px-5 pb-3 pt-2 text-gray-800 text-md'>1 double bed</p>
+                </div>
+                <div>
                 </div>
             </div>
         </>
